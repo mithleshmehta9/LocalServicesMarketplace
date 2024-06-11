d6a5d09779be4424cf3d364de0e0f9464a490261
Mithlesh Mehta <mehtamithlesh9@gmail.com>
2024-06-11 23:02:55 +0545

    Initial Commit

---
+++ LocalServicesMarketplace/LocalServicesMarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalServicesMarketplaceApp.swift
+//  LocalServicesMarketplace
+//
+//  Created by Mithlesh Mehta on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalServicesMarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
